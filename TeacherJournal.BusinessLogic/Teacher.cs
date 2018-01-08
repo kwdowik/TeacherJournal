d2577b45698199ef9d1bb9c564e0427212833efc
kwdowik <wdowikkacper@gmail.com>
2018-01-08 18:34:44 +0100

    Changed Teacher model

--- TeacherJournal.BusinessLogic/Teacher.cs
+++ TeacherJournal.BusinessLogic/Teacher.cs
@@ -4,17 +4,9 @@ namespace TeacherJournal.BusinessLogic
 {
     public class Teacher
     {
-        public Teacher(int id, string login, string password, string subject)
-        {
-            TeacherId = id;
-            Login = login;
-            Password = password;
-            Subject = subject;
-        }
-        private Teacher() {}
+        public Teacher() {}
         public int TeacherId {get; set;}
         public string Login {get; set;}
         public string Password {get; set;}
-        public string Subject {get; set;}
     }
 }
